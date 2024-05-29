9dc5b7ad765b08ec8032f4dbf907467586d31237
Caio Vieira <cravieira@inf.ufrgs.br>
2024-05-28 21:49:12 -0300

    Fix wrong package name for pytorch

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@
 from setuptools import setup
 
 setup(name='nvsa', version='0.1', description='NVSA frontend and backend implementation',
-      url='http://github.com/IBM/neuro-vector-symbolic-architectures', author='Michael Hersche', 
-      packages=['nvsa'], install_requires=['pytorch'], zip_safe=False)
+      url='http://github.com/IBM/neuro-vector-symbolic-architectures', author='Michael Hersche',
+      packages=['nvsa'], install_requires=['torch'], zip_safe=False)
